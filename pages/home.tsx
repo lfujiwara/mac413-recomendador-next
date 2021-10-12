814215914ec3d9438ca8d10419de22d50c1182a1
Lucas <lucastfujiwara@gmail.com>
2021-10-11 22:51:04 -0300

    atualizar pagina para gerenciar disciplinas

--- pages/home.tsx
+++ pages/home.tsx
@@ -12,17 +12,29 @@ import {
 } from "../lib/views/gerenciador-disciplinas-cursadas-view";
 
 import { Header } from "../components/Header";
+import { Input } from "@chakra-ui/input";
 
 export const GerenciadorDisciplinasCursadas = () => {
   const view = useGerenciadorDisciplinasCursadasView();
 
   return (
-    <Box>
+    <Box pt="4">
+      <Input
+        placeholder="Buscar disciplina"
+        value={view.termoDeBuscaDisciplinas}
+        onChange={(evt) =>
+          view.atualizarTermoDeBuscaDisciplinas(evt.target.value)
+        }
+        mb="2"
+      />
       <SimpleGrid minChildWidth="200px" spacing="4">
         {view.disciplinasAExibir.map((disciplina) => (
           <Box
             key={disciplina.codigo}
             p="4"
+            transition="linear"
+            transitionProperty="all"
+            transitionDuration="300ms"
             border={
               view.disciplinaFoiCursada(disciplina.codigo)
                 ? "4px solid green"
@@ -34,10 +46,29 @@ export const GerenciadorDisciplinasCursadas = () => {
                 : () => view.adicionarDisciplina(disciplina.codigo)
             }
           >
-            <VStack>
+            <VStack h="full">
               <Text textAlign="center">{disciplina.codigo}</Text>
               <Text textAlign="center" fontSize="sm">
                 {disciplina.nome}
+              </Text>
+              <Box flex="1" />
+              <Text
+                p="2"
+                fontSize="xs"
+                bgColor={
+                  view.disciplinaFoiCursada(disciplina.codigo)
+                    ? "green"
+                    : "gray"
+                }
+                color="white"
+                rounded="md"
+                transition="linear"
+                transitionProperty="all"
+                transitionDuration="300ms"
+              >
+                {view.disciplinaFoiCursada(disciplina.codigo)
+                  ? "CURSADA"
+                  : "NÃO CURSADA"}
               </Text>
             </VStack>
           </Box>
